fdaced6be5b34f762321e950687eb853eadf9076
daisuke iba <d.iba@futurek.com>
2025-09-04 17:00:52 +0900

    init

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import { SITE } from './src/constants';
+
+const dropOptions = process.env.NODE_ENV === 'development'
+  ? []
+  : ['console', 'debugger'];
+
+export default defineConfig({
+  site: SITE.DOMAIN,
+  base: SITE.BASE,
+  outDir: `./dist${SITE.BASE}/`,
+  trailingSlash: 'always',
+  vite: {
+    esbuild: {
+      drop: dropOptions,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            $base: '${SITE.BASE}';
+            @use './src/styles/variables' as *;
+          `
+        }
+      }
+    }
+  },
+});
