62ded8f878847cf74026f7b0439fbe20c1e1a54f
daisuke iba <d.iba@futurek.com>
2025-09-04 17:07:42 +0900

    add: d&d

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
-const BASE: string = '/model-viewer-preview';
+const BASE: string = 'model-viewer-preview';
 
 export const SITE = {
-  ROOT: `${import.meta.env.PUBLIC_DOMAIN}${BASE}/`,
+  ROOT: `${import.meta.env.PUBLIC_DOMAIN}/${BASE}/`,
   DOMAIN: import.meta.env.PUBLIC_DOMAIN,
   BASE: BASE,
 }
